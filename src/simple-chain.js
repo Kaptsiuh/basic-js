2bf20f410597952bfd5b2eed14355e225041d4a2
Dima <dimaberry1@gmail.com>
2022-10-18 00:19:35 +0300

    feat: complete task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -8,28 +8,43 @@ const { NotImplementedError } = require("../extensions/index.js");
 const chainMaker = {
   chainArray: [],
   getLength() {
-    throw new NotImplementedError("Not implemented");
+    // throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
-    return chainMaker.chainArray.length;
+    return this.chainArray.length;
   },
   addLink(value) {
-    throw new NotImplementedError("Not implemented");
+    // throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
-    // chainMaker.chainArray.push(value);
+    this.chainArray.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw new NotImplementedError("Not implemented");
+    // throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
-    chainMaker.chainArray.splice(position, 1);
+    if (
+      position > 0 &&
+      position <= this.chainArray.length - 1 &&
+      typeof position === "number"
+    ) {
+      this.chainArray.splice(position - 1, 1);
+      return this;
+    }
+    this.chainArray = [];
+
+    throw new Error("You can't remove incorrect link!");
   },
   reverseChain() {
-    throw new NotImplementedError("Not implemented");
+    // throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
-    chainMaker.chainArray.reverse();
+    this.chainArray.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError("Not implemented");
+    // throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
+    let result = this.chainArray.join("~~");
+    this.chainArray = [];
+    return result;
   },
 };
 
@@ -38,10 +53,28 @@ module.exports = {
 };
 
 // console.log(chainMaker.addLink(1).addLink(2));
+
 // console.log(chainMaker.chainArray);
+
 // console.log(
 //   chainMaker.addLink(1).addLink(2).removeLink(1).addLink(3).finishChain()
 // );
+
 // console.log(
 //   chainMaker.addLink(1).addLink(2).reverseChain().addLink(3).finishChain()
+// );
+
+// console.log(
+//   chainMaker
+//     .addLink("GHI")
+//     .addLink(null)
+//     .reverseChain()
+//     .addLink(333)
+//     .reverseChain()
+//     .reverseChain()
+//     .addLink(0)
+//     .reverseChain()
+//     .reverseChain()
+//     .addLink("GHI")
+//     .finishChain()
 // );
