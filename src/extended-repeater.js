2bc3d9b08d14db2a6962a07aa9f3a28337831d7c
Dima <dimaberry1@gmail.com>
2022-10-17 23:18:56 +0300

    feat: complete task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,10 +16,88 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 function repeater(str, options) {
-  throw new NotImplementedError("Not implemented");
+  // throw new NotImplementedError("Not implemented");
   // remove line with error and write your code here
+  str = String(str);
+  if (!options.hasOwnProperty("repeatTimes")) {
+    options.repeatTimes = 1;
+  }
+  if (!options.hasOwnProperty("separator")) {
+    options.separator = "+";
+  }
+  if (options.hasOwnProperty("additionRepeatTimes")) {
+    if (!options.hasOwnProperty("additionSeparator")) {
+      options.additionSeparator = "|";
+    }
+  }
+  if (options.hasOwnProperty("addition")) {
+    options.addition = String(options.addition);
+  }
+  let result = [];
+  for (let i = 0; i < options.repeatTimes; i++) {
+    result.push(str);
+    if (!options.hasOwnProperty("additionRepeatTimes")) {
+      result.push(options.addition);
+    }
+    for (let j = 0; j < options.additionRepeatTimes; j++) {
+      result.push(options.addition);
+      if (
+        j + 1 !== options.additionRepeatTimes &&
+        options.additionRepeatTimes > 1
+      ) {
+        result.push(options.additionSeparator);
+      }
+    }
+    if (i + 1 !== options.repeatTimes) {
+      result.push(options.separator);
+    }
+  }
+  result = result.filter((e) => e != undefined);
+  return result.join("");
 }
 
 module.exports = {
   repeater,
 };
+
+// console.log(
+//   repeater("la", {
+//     repeatTimes: 3,
+//     separator: "s",
+//     addition: "+",
+//     additionRepeatTimes: 1,
+//   })
+// );
+
+// console.log(
+//   repeater("TESTstr", {
+//     separator: "ds",
+//     addition: "ADD!",
+//     additionSeparator: ")))000",
+//   })
+// );
+
+// console.log(repeater("la", { repeatTimes: 3 }));
+
+// console.log(
+//   repeater("12345", { repeatTimes: 5, separator: "3 words separator" })
+// );
+
+// console.log(
+//   repeater("REPEATABLE_STRING", {
+//     repeatTimes: 2,
+//     separator: "222",
+//     addition: "ADDITION",
+//     additionRepeatTimes: 3,
+//   })
+// );
+
+// console.log(
+//   repeater(null, {
+//     repeatTimes: 3,
+//     separator: "??? ",
+//     addition: null,
+//     additionRepeatTimes: 3,
+//     additionSeparator: "!!!",
+//   })
+// );
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,83 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
-    return;
+  constructor(method = "true") {
+    this._type = method;
   }
-  decrypt() {
-    throw new NotImplementedError("Not implemented");
+
+  alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  encrypt(message, key) {
+    // throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
+    if (!message || !key) throw new Error("Incorrect arguments!");
+
+    let koef = Math.ceil(message.length / key.length);
+    key = key.repeat(koef).toUpperCase();
+    message = message.toUpperCase();
+    let keyArr = key.split("");
+    let result = [];
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].charCodeAt() < 65 || message[i].charCodeAt() > 90) {
+        result.push(message[i]);
+        keyArr.splice(i, 0, " ");
+      } else {
+        let letterIndex = 0;
+        for (let j = 0; j < this.alphabet.length; j++) {
+          if (this.alphabet[j] === message[i]) {
+            letterIndex = j;
+          }
+        }
+        let shift = 0;
+        for (let k = 0; k < this.alphabet.length; k++) {
+          if (this.alphabet[k] == keyArr[i]) {
+            shift = k;
+          }
+        }
+        let code = (letterIndex + shift) % 26;
+
+        result.push(this.alphabet[code]);
+      }
+    }
+    return this._type ? result.join("") : result.reverse().join("");
+  }
+  decrypt(message, key) {
+    // throw new NotImplementedError("Not implemented");
+    // remove line with error and write your code here
+    if (!message || !key) throw new Error("Incorrect arguments!");
+
+    let koef = Math.ceil(message.length / key.length);
+    key = key.repeat(koef).toUpperCase();
+    message = message.toUpperCase();
+    let keyArr = key.split("");
+    let result = [];
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].charCodeAt() < 65 || message[i].charCodeAt() > 90) {
+        result.push(message[i]);
+        keyArr.splice(i, 0, " ");
+      } else {
+        let letterIndex = 0;
+        for (let j = 0; j < this.alphabet.length; j++) {
+          if (this.alphabet[j] === message[i]) {
+            letterIndex = j;
+          }
+        }
+        let shift = 0;
+        for (let k = 0; k < this.alphabet.length; k++) {
+          if (this.alphabet[k] == keyArr[i]) {
+            shift = k;
+          }
+        }
+        let code = letterIndex - shift;
+        if (code < 0) {
+          code = 26 + code;
+        }
+        result.push(this.alphabet[code]);
+      }
+    }
+    return this._type ? result.join("") : result.reverse().join("");
   }
 }
 
@@ -36,8 +105,18 @@ module.exports = {
 };
 
 // console.log(
-//   VigenereCipheringMachine.encrypt(
+//   new VigenereCipheringMachine().encrypt(
 //     "ICWWQAM KECEIK JVZZT EADGG!",
 //     "rollingscopes"
 //   )
+// );
+
+// console.log(new VigenereCipheringMachine().encrypt("HSVD AJAL ^^", "behappy"));
+
+// console.log(
+//   new VigenereCipheringMachine().decrypt("TRVVFB VT JSUIFMYL!", "learning")
+// );
+
+// console.log(
+//   new VigenereCipheringMachine().encrypt("Same length key", "Samelengthkey")
 // );
